79e819e6730c2cf0766b2a8c9e3cdce9eddaa926
saki.ota <saki.a.ota@rakuten.com>
2018-06-01 05:27:56 +0000

    resub4

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -6,5 +6,10 @@
     <p>{{ $task->content }}</p>
 
     {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id]) !!}
+    
+        {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
 
 @endsection
