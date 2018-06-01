d2ef3d65498a351afb278f46a73635a15b48b2e9
saki.ota <saki.a.ota@rakuten.com>
2018-06-01 05:10:40 +0000

    fix sub3

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,7 +8,6 @@
 
         <ul>
             @foreach ($tasks as $task)
-                <li>{{ $task->content }}</li>
                 <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
 
             @endforeach
